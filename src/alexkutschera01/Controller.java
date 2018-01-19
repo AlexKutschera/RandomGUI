a0129deb61c2e2ad61a75bd4a8cd58a6622b3d12
AlexKutschera <alexkutschera01@gmail.com>
2018-01-19 19:24:41 +0100

    Initial Commit

---
+++ src/alexkutschera01/Controller.java
@@ -0,0 +1,13 @@
+package alexkutschera01;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    public TextField min;
+    public TextField max;
+    public TextField search;
+    public Label output;
+    public Button run;
+}
---
+++ src/alexkutschera01/Main.java
@@ -0,0 +1,79 @@
+package alexkutschera01;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Random;
+
+public class Main extends Application {
+
+    private int min = 0, max = 0, searched;
+    private Controller controller;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("main.fxml"));
+        Parent root = loader.load();
+        controller = loader.getController();
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+        controller.min.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (isInteger(newValue)){
+                min = Integer.parseInt(newValue);
+            } else {
+                controller.min.textProperty().setValue("");
+            }
+        });
+        controller.max.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (isInteger(newValue)){
+                max = Integer.parseInt(newValue);
+            } else {
+                controller.max.textProperty().setValue("");
+            }
+        });
+        controller.search.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (isInteger(newValue) && Integer.parseInt(newValue) >= min && Integer.parseInt(newValue) <= max){
+                searched = Integer.parseInt(newValue);
+            } else {
+                controller.search.textProperty().setValue("");
+            }
+        });
+        controller.run.setOnAction(event -> run());
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private static boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+        } catch(NumberFormatException | NullPointerException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private void run(){
+        controller.run.setDisable(true);
+        int resultsCount = 0;
+        long startTime = System.currentTimeMillis();
+        Random random = new Random();
+        int result;
+        while ((result = (min + random.nextInt(max - min))) != searched){
+            System.out.print("\r" + result);
+            resultsCount++;
+        }
+        long runTime = System.currentTimeMillis() - startTime;
+        controller.output.textProperty().setValue(
+                "Runtime: " + runTime + "\n" +
+                        "Randoms: " + resultsCount
+        );
+        controller.run.setDisable(false);
+    }
+}
